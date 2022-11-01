b0ddd4f9ef2781a46fecb433674869b156df7623
Tiffany Barker <Tiffany.Barker13@yahoo.com>
2022-10-31 20:31:16 -0400

    commit tree cd44e3d17bc405c66507243a033065c2220c58be

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+    // todo
+function whisper(string) {
+    return string.toLowercase();
+}
+    //todo
+function whisper(string) {
+    return (string).toLowercase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+    //todo
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+    //todo
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+    //todo
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toLowerCase())  {
+   return "I can't hear you!" 
+    }
+    if (string === string.toUpperCase()){
+        return "YES INDEED!"
+    }
+    if (string === string.toUpperCase()){
+        return "Let's have dinner together!"   
+    }
+        return "I would love to!"
+}
